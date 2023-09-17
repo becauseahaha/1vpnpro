5d839e82e9b865ecefc0602d0121971793be5f07
Andrey Zhelezko <because.ahaha@gmail.com>
2023-09-18 02:24:24 +0300

    client fixes

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+if (window.innerWidth <= 425 && Swiper !== undefined) {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 28,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        }
+    });
+}
+
+document.querySelectorAll('.js-scroll-to-buy').forEach(el => {
+    el.addEventListener('click', (e) => {
+        document.getElementById('buy').scrollIntoView({ behavior: 'smooth', block: 'center'});
+    })
+});
+
+document.querySelectorAll('.popup__close').forEach(el => {
+    el.addEventListener('click', (e) => {
+        e.target.closest('.popup').classList.remove('is-shown');
+        window.location.hash = "";
+    })
+});
+
+if (window.location.hash == '#popup-success') {
+    document.getElementById('popup-success').classList.add('is-shown');
+}
+if (window.location.hash == '#popup-fail') {
+    document.getElementById('popup-fail').classList.add('is-shown');
+}
+
+var rellax = new Rellax('.js-parallax', {
+    breakpoints: [425, 1024, 1920],
+    center: true
+});
